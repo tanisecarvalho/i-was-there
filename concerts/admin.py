2a4729abc0b3c748abb16287ed807a4077f08f88
Tanise Carvalho <carvalhotanise@gmail.com>
2023-08-13 19:00:56 +0100

    feat: add admin panel and summernote

--- concerts/admin.py
+++ concerts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Concert, Band, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('sentence')
+
+
+admin.site.register(Concert)
+admin.site.register(Band)
--- iwasthere/settings.py
+++ iwasthere/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'concerts',
 ]
 

e08cafbc778dbd54671e0d6f16f592848826628d
Tanise Carvalho <carvalhotanise@gmail.com>
2023-08-13 19:22:33 +0100

    feat: add search and display views on admin panel

--- concerts/admin.py
+++ concerts/admin.py
@@ -6,8 +6,20 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
 
+    list_display = ('concert', 'user', 'created_on')
+    search_fields = ['sentence']
     summernote_fields = ('sentence')
 
 
-admin.site.register(Concert)
-admin.site.register(Band)
+@admin.register(Concert)
+class ConcertAdmin(SummernoteModelAdmin):
+
+    list_display = ('band', 'date', 'user', 'slug')
+    search_fields = ['country', 'city', 'date']
+    prepopulated_fields = {'slug': ('date', 'country', 'band', )}
+
+
+@admin.register(Band)
+class BandAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'user', 'created_on')
+    search_fields = ['name']

ed31ab286f7c8459c7d8658f67bbc00b2cf21001
Tanise Carvalho <carvalhotanise@gmail.com>
2023-08-14 16:45:08 +0100

    feat: set debug false to production

--- iwasthere/settings.py
+++ iwasthere/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['i-was-there-d5ba14a8429f.herokuapp.com', '127.0.0.1']
 

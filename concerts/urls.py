805b3e196b7c0df4986f77d1a944cb256f996c2e
Tanise Carvalho <carvalhotanise@gmail.com>
2023-08-22 19:28:11 +0100

    feat: add url to view concert page

--- concerts/urls.py
+++ concerts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.ConcertList.as_view(), name='home')
+    path('', views.ConcertList.as_view(), name='home'),
+    path('<slug:slug>/', views.ConcertDetail.as_view(), name='concert_detail'),
 ]
--- concerts/views.py
+++ concerts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Concert
 
 
@@ -8,3 +8,20 @@ class ConcertList(generic.ListView):
     queryset = Concert.objects.order_by('-created_on')
     template_name = 'concerts.html'
     paginate_by = 6
+
+
+class ConcertDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Concert.objects
+        concert = get_object_or_404(queryset, slug=slug)
+        comments = concert.comments.order_by('created_on')
+
+        return render(
+            request,
+            "view_concert.html",
+            {
+                "concert": concert,
+                "comments": comments
+            },
+        )
